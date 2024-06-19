bd1648fb7795cd4686112e1c5b619f2e54431e04
Allyson Alves <85911289+AllysonAlves1@users.noreply.github.com>
2024-06-18 23:00:39 -0300

    feat: desafio sistema bancario

---
+++ desafios/sistema-bancario.py
@@ -0,0 +1,66 @@
+menu = """
+
+[d] Depositar
+[s] Sacar
+[e] Extrato
+[q] Sair
+
+=> """
+
+saldo = 0
+limite = 500
+extrato = ""
+limite_saques = 3
+
+while True:
+
+    opcao = input(menu)
+
+    match opcao:
+        case "d":
+            valor = float(input("Informe o valor do depósito: "))
+
+            if valor > 0:
+                saldo += valor
+                extrato += f"Depósito: R$ {valor:.2f}\n"
+
+            else:
+                print("Operação falhou! O valor informado é inválido.")
+
+        case "s":
+            valor = float(input("Informe o valor do saque: "))
+
+            excedeu_saldo = valor > saldo
+
+            excedeu_limite = valor > limite
+
+            excedeu_saques = limite_saques == 0
+
+            if excedeu_saldo:
+                print("Operação falhou! Você não tem saldo suficiente.")
+
+            elif excedeu_limite:
+                print("Operação falhou! O valor do saque excede o limite.")
+
+            elif excedeu_saques:
+                print("Operação falhou! Número máximo de saques excedido.")
+
+            elif valor > 0:
+                saldo -= valor
+                extrato += f"Saque: R$ {valor:.2f}\n"
+                limite_saques -= 1
+
+            else:
+                print("Operação falhou! O valor informado é inválido.")
+
+        case "e":
+            print("\n================ EXTRATO ================")
+            print("Não foram realizadas movimentações." if not extrato else extrato)
+            print(f"\nSaldo: R$ {saldo:.2f}")
+            print("==========================================")
+
+        case "q":
+            break
+
+        case _:
+            print("Operação inválida, por favor selecione novamente a operação desejada.")
